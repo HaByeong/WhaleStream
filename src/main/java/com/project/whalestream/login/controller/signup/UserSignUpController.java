a553f3bea01c4c40bc61f68dbcded4fc631d404c
HaByeong <121917642+HaByeong@users.noreply.github.com>
2025-06-03 17:27:44 +0900

    feat: 회원 가입 API 반환 객체 변경

--- src/main/java/com/project/whalestream/login/controller/signup/UserSignUpController.java
+++ src/main/java/com/project/whalestream/login/controller/signup/UserSignUpController.java
@@ -3,6 +3,7 @@ package com.project.whalestream.login.controller.signup;
 import com.project.whalestream.login.dto.signup.UserSignUpRequestDto;
 import com.project.whalestream.login.service.signup.UserSignUpServiceInterface;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,10 @@ public class UserSignUpController {
     private final UserSignUpServiceInterface userSignUpService;
 
     @PostMapping
-    public String userSignUp(@RequestBody UserSignUpRequestDto signupRequestDtoUser) {
+    public ResponseEntity userSignUp(@RequestBody UserSignUpRequestDto signupRequestDtoUser) {
         //회원가입 로직 처리
         userSignUpService.newUser(signupRequestDtoUser);
-        return "회원 가입 완료";
+        return ResponseEntity.ok("회원 가입 완료");
     }
 
 }
